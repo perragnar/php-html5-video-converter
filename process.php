663b90df6e07dfa78f54f44f87cafa8ce24dd319
Per Ragnar Edin <perragnar@gmail.com>
2017-10-18 09:20:01 +0200

    Add a player to play the converted video

--- process.php
+++ process.php
@@ -1,17 +1,18 @@
 <?php 
-$uploads_dir = "original/";
-$file_name = basename($_FILES["file"]["name"]);
+$uploads_dir = 'original/';
+$file_name = basename($_FILES['file']['name']);
 $output_name = explode('.', $file_name)[0];
 $uploaded_file = $uploads_dir . $file_name;
 
-if(isset($_POST["submit"])) {
-  if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploaded_file)) {
+if(isset($_POST['submit'])) {
+  if(move_uploaded_file($_FILES['file']['tmp_name'], $uploaded_file)) {
     // Make sure to get the correct path to ffmpeg
     // Run $ where ffmpeg to get the path
-    $ffmpeg = "/usr/local/bin/ffmpeg";
+    $ffmpeg = '/usr/local/bin/ffmpeg';
     
     // MP4
-    exec($ffmpeg . ' -i "' . $uploaded_file . '" -r 25 -c:v libx264 "./converted/' . $output_name . '.mp4" -y 2>&1', $output, $convert);
+    $video_mp4 = $output_name . '.mp4';
+    exec($ffmpeg . ' -i "' . $uploaded_file . '" -r 25 -c:v libx264 "./converted/' . $video_mp4 . '" -y 2>&1', $output, $convert);
 
     // Debug
     // echo '<pre>' . print_r($output, 1) . ' </pre>';
@@ -21,7 +22,8 @@ if(isset($_POST["submit"])) {
     echo '</div>';
 
     // WebM
-    exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libvpx -r 25 -c:a libvorbis "./converted/' . $output_name . '.webm" -y 2>&1', $output, $convert);
+    $video_webm = $output_name . '.webm';
+    exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libvpx -r 25 -c:a libvorbis "./converted/' . $video_webm . '" -y 2>&1', $output, $convert);
 
     // Debug
     // echo '<pre>' . print_r($output, 1) . ' </pre>';
@@ -31,3 +33,10 @@ if(isset($_POST["submit"])) {
     echo '</div>';
   }
 }
+
+?>
+
+<video controls autoplay loop>
+  <source src="./converted/<?= $video_mp4; ?>" type="video/mp4">;   
+  <source src="./converted/<?= $video_webm; ?>" type="video/webm">
+</video>
